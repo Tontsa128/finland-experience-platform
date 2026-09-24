9490c6a77a88c1eef73ef265c868fd5e6c9d4565
Tontsa128 <toni.jukka4@gmail.com>
2026-09-24 18:49:51 +0300

    fix: allow Visit Finland image CDN

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 /** @type {import("next").NextConfig} */
-const nextConfig = { reactStrictMode: true, poweredByHeader: false, images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }, { protocol: "https", hostname: "res.cloudinary.com" }] } };
+const nextConfig = { reactStrictMode: true, poweredByHeader: false, images: { remotePatterns: [{ protocol: "https", hostname: "images.unsplash.com" }, { protocol: "https", hostname: "res.cloudinary.com" }, { protocol: "https", hostname: "cdn-datahub.visitfinland.com" }] } };
 export default withNextIntl(nextConfig);
