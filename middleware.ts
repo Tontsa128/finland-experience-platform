96336031e13fd22a19806ad6becece39e65b0486
Tontsa128 <toni.jukka4@gmail.com>
2026-09-24 19:00:35 +0300

    fix: narrow localized middleware cookie type

--- middleware.ts
+++ middleware.ts
@@ -66,7 +66,7 @@ function setLocaleCookie(response: NextResponse, locale: string) {
 function handleLocalizedRequest(req: NextRequest) {
   const response = handleI18n(req);
   const localeMatch = req.nextUrl.pathname.match(/^\/(fi|es|en)(?:\/|$)/);
-  if (localeMatch) return setLocaleCookie(response, localeMatch[1] ?? defaultLocale);
+  if (localeMatch) {\n    const locale = (localeMatch[1] ?? defaultLocale) as (typeof locales)[number];\n    return setLocaleCookie(response, locale);\n  }
   return response;
 }
 
